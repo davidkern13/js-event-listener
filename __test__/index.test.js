f6966242dc05ba06fdaf86a13ee0d4d44c72d46f
David Kern <david33belle@gmail.com>
2021-12-10 17:31:07 +0200

    Add testings :white_check_mark: & fixing code :adhesive_bandage:

---
+++ __test__/index.test.js
@@ -0,0 +1,51 @@
+import { addEventListener, removeEventListener, removeAllEventListener, emitEvent} from '../index';
+
+describe('Js event listener tests', () => {
+    const _addEventListener = (name = 'test', func = () => {}) => {
+        return addEventListener(name, func);
+    }
+
+    test('test _addEventListener are added properly', () => {
+        const id = _addEventListener();
+        expect(id).toBe(1);
+    });
+
+    test('test _addEventListener not to be added', () => {
+        const id = _addEventListener(1);
+        expect(id).toBe(false);
+    });
+
+    test('test emitEvent work properly', () => {
+        const myMockFn = jest.fn();
+        
+        let mockdata = {value: ['test']};
+        myMockFn(mockdata);
+
+        _addEventListener('test1', myMockFn);
+
+        emitEvent('test1', mockdata);
+        expect(myMockFn).toHaveBeenCalledWith(mockdata);
+    });
+
+    test('test removeEventListener remove event properly', () => {
+        let id = _addEventListener();
+
+        let removerId = removeEventListener(id);
+        expect(removerId).toBe(true);
+    });
+
+    test('test removeEventListener not removed event', () => {
+        let removedId = removeEventListener({});
+        expect(removedId).toBe(false);
+    });
+
+    test('test removeAllEventListener remove all events properly', () => {
+        _addEventListener();
+
+        let removedEvents = removeAllEventListener();
+        expect(removedEvents).toBe(true);
+    });
+
+
+});
+
--- index.js
+++ index.js
@@ -1,104 +1,117 @@
 /**
 * Class-based object what represent id and events values
 */
-class ReactNativeEventRegister {
-    static id = 0;
-    static events = Object.create(null);
-}
 
-/**
-* Function add event to event object
-* @param    {String} Name of the event
-* @return   {Int} Id of created event 
-*/
-function addEventListener(name, handler){
-    ReactNativeEventRegister.id++;
-    let _id = ReactNativeEventRegister.id;
-    let _events = ReactNativeEventRegister.events;
-    let isValidName = checkName(name);
-    let isValidhandler = checkHandler(handler);
-    
-    if(isValidName && isValidhandler){
-        let data = {
-            name,
-            handler
-        };
-        _events[_id] = data;
-    }else{
-        return false;
-    } 
-
-    return _id;
-}
-
-/**
-* Function remove from event object by id
-* @param    {Id} Id of event to be removed
-* @return   {Boolean} 
-*/
-function removeEventListener(id){
-    let _events = ReactNativeEventRegister.events;
-    if(checkId(id)){
-        return delete _events[id];
-    }
-    return false;
-}
-/**
-* Function remove all events
-* @return   {Boolean} 
-*/
-function removeAllEventListener(){
-    let _id = ReactNativeEventRegister.id = 0;
-    let _events = ReactNativeEventRegister.events.length = 0;
-
-    return !_id && !_events ? true : false;
-}
-
-/**
-* Function remove from event object by id
-* @param    {Id,String} Id or String to remove from event object
-* @return   {Boolean} 
-*/
-function emitEvent(identity, data){
-    let _events = ReactNativeEventRegister.events;
-
-    if (!data || !data.length) data = [];
-
-    if(checkName(identity)){
-        emitByEventName(identity, _events, data);
-    }
-    if(checkId(identity)){
-        emitByEventId(identity, _events, data);
-    }
-}
-
-const emitByEventName = (name, _events, data) => {
-    Object.keys(_events).forEach( (id) => {
-        if(_events[id] && _events[id].name === name){
-            _events[id].handler(data);
-        }
-    });
-}
-
-const emitByEventId = (id, _events, data) => {
-    _events[id].handler(data);
-}
-
-const checkName = (name) => {
-    return (typeof name === "string");
-}
-
-const checkHandler = (handler) => {
-    return (typeof handler === "function");
-}
-
-const checkId = (id) => {
-    return (typeof id === "number");
-}
-
-module.exports = {
-    addEventListener,
-    removeEventListener,
-    removeAllEventListener,
-    emitEvent
-}
+ class ReactNativeEventRegister {
+     static id = 0;
+     static events = Object.create(null);
+ }
+ 
+ /**
+ * Function add event to event object
+ * @param    {String} Name of the event
+ * @return   {Int} Id of created event 
+ */
+ function addEventListener(name, handler){
+     ReactNativeEventRegister.id++;
+     let _id = ReactNativeEventRegister.id;
+     let _events = ReactNativeEventRegister.events;
+     let isValidName = checkName(name);
+     let isValidhandler = checkHandler(handler);
+     
+     if(isValidName && isValidhandler){
+         let data = {
+             name,
+             handler
+         };
+         _events[_id] = data;
+     }else{
+         return false;
+     } 
+ 
+     return _id;
+ }
+ 
+ /**
+ * Function remove from event object by id
+ * @param    {Identity} should be id or event name event to be removed
+ * @return   {Boolean} 
+ */
+ function removeEventListener(identity){
+     let _events = ReactNativeEventRegister.events;
+     if(checkId(identity)){
+         return deleteEventById(identity, _events);
+     }
+     if(checkName(identity)){
+         let id = findEventByName(identity, _events);
+         if(id) return deleteEventById(identity, _events);
+     }
+     return false;
+ }
+ /**
+ * Function remove all events
+ * @return   {Boolean} 
+ */
+ function removeAllEventListener(){
+     let _id = ReactNativeEventRegister.id = 0;
+     let _events = ReactNativeEventRegister.events.length = 0;
+ 
+     return !_id && !_events ? true : false;
+ }
+ 
+ /**
+ * Function remove from event object by id
+ * @param    {Id,String} Id or String to remove from event object
+ * @return   {Boolean} 
+ */
+ function emitEvent(identity, data){
+     let _events = ReactNativeEventRegister.events;
+ 
+     if (!data || !data.length) data = [];
+ 
+     if(checkName(identity)){
+         let id = findEventByName(identity, _events);
+         if(id)_events[id].handler(data);
+     }
+     if(checkId(identity)){
+         emitByEventId(identity, _events, data);
+     }
+ }
+ 
+ /**
+ * UTILS FUNCTIONS
+ */
+ 
+ const emitByEventId = (id, _events, data) => {
+     _events[id].handler(data);
+ }
+ 
+ const findEventByName = (_name, _events) => {
+     for(let [id, name] of Object.entries(_events)){
+         if(_events[id] && name === name) return id;
+     }
+     return false;
+ }
+ 
+ const deleteEventById = (id, _events) => {
+     return delete _events[id];
+ }
+ 
+ const checkName = (name) => {
+     return (typeof name === "string");
+ }
+ 
+ const checkHandler = (handler) => {
+     return (typeof handler === "function");
+ }
+ 
+ const checkId = (id) => {
+     return (typeof id === "number");
+ }
+ 
+ module.exports = {
+     addEventListener,
+     removeEventListener,
+     removeAllEventListener,
+     emitEvent
+ }
